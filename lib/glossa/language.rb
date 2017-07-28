e46fe6999e41ea31e32b4dc208c553efed9082b5
Jake Franklin <jacob.d.franklin@gmail.com>
2017-07-28 10:22:36 -0700

    Bug fixing

--- lib/glossa/language.rb
+++ lib/glossa/language.rb
@@ -39,23 +39,27 @@ class Glossa::Language
                 "F" => "mn",
                 "L" => "rl"
             }
-            @structure  = options['structure']     || "CVC"
-            @exponent   = options['exponent'].to_i || 2
-            @restricts  = options['restricts']     || []
-            @cortho     = options['cortho']        || {}
-            @vortho     = options['vortho']        || {}
-            @noortho    = options['noortho']       || true
-            @nomorph    = options['nomorph']       || true
-            @nowordpool = options['nowordpool']    || true
-            @minsyll    = options['minsyll'].to_i  || 1
-            @maxsyll    = options['maxsyll'].to_i  || 1
-            @morphemes  = options['morphemes']     || {}
-            @words      = options['words']         || {}
-            @names      = options['names']         || []
-            @joiner     = options['joiner']        || ' '
-            @maxchar    = options['maxchar'].to_i  || 12
-            @minchar    = options['minchar'].to_i  || 5
+            @structure  = options['structure']  || "CVC"
+            @exponent   = options['exponent']   || 2
+            @restricts  = options['restricts']  || []
+            @cortho     = options['cortho']     || {}
+            @vortho     = options['vortho']     || {}
+            @noortho    = options['noortho']    || true
+            @nomorph    = options['nomorph']    || true
+            @nowordpool = options['nowordpool'] || true
+            @minsyll    = options['minsyll']    || 1
+            @maxsyll    = options['maxsyll']    || 1
+            @morphemes  = options['morphemes']  || {}
+            @words      = options['words']      || {}
+            @names      = options['names']      || []
+            @joiner     = options['joiner']     || ' '
+            @maxchar    = options['maxchar']    || 12
+            @minchar    = options['minchar']    || 5
         end
+
+        # Ensure correct data types are used if options are passed in incorrectly
+        int_keys = [@exponent, @minsyll, @maxsyll, @maxchar, @minchar]
+        int_keys.each { |k| if !k.kind_of?(Integer) then k = k.to_i end }
 
         @genitive   = get_morpheme('of')
         @definitive = get_morpheme('the')
@@ -147,7 +151,7 @@ class Glossa::Language
                     end
                     next
                 end
-                
+
                 syll << choose(self.phonemes[ptype], self.exponent)
             end
 
